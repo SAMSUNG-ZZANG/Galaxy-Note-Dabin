2445020be27601ef73c2b831decf63e519396e83
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-04-10 23:22:12 +0900

    [ADD] #3 - Add Util

---
+++ core/src/main/java/org/sopt/core/util/CoilImageLoader.kt
@@ -0,0 +1,17 @@
+package org.sopt.core.util
+
+import android.content.Context
+import android.os.Build.VERSION.SDK_INT
+import coil.ImageLoader
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+
+fun Context.getGifLoader() = ImageLoader.Builder(this)
+    .components {
+        if (SDK_INT >= 28) {
+            add(ImageDecoderDecoder.Factory())
+        } else {
+            add(GifDecoder.Factory())
+        }
+    }
+    .build()
--- core/src/main/java/org/sopt/core/util/ext/ContextExt.kt
+++ core/src/main/java/org/sopt/core/util/ext/ContextExt.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Context.INPUT_METHOD_SERVICE
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 
 fun Context.showKeyboard(view : View) {
     val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
@@ -13,4 +14,8 @@ fun Context.showKeyboard(view : View) {
 fun Context.closeKeyboard(view : View) {
     val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.showSoftInput(view.findFocus(), 0)
+}
+
+fun Context.shortToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
