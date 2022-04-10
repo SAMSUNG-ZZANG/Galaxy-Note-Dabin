018dc5b70059817c227f373fe0b67b4cd2d36c9f
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-04-10 23:32:35 +0900

    [UI] #3 - Add Pika

--- feature/home/src/main/java/org/sopt/home/HomeActivity.kt
+++ feature/home/src/main/java/org/sopt/home/HomeActivity.kt
@@ -1,16 +1,11 @@
 package org.sopt.home
 
 import android.os.Bundle
-import coil.load
-import coil.transform.CircleCropTransformation
 import org.sopt.core.base.BindingActivity
 import org.sopt.home.databinding.ActivityHomeBinding
 
 class HomeActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding.ivOwner.load(R.raw.gif_guy_run) {
-            transformations(CircleCropTransformation())
-        }
     }
 }
